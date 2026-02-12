235307bfaa312ace83a940f35f5d26fed0ac3745
daniel alejandro neira <danielgrc85@gmail.com>
2026-02-11 21:07:55 -0500

    Adicion de dependencias

--- src/main/java/co/edu/cesde/pps/model/Address.java
+++ src/main/java/co/edu/cesde/pps/model/Address.java
@@ -2,6 +2,7 @@ package co.edu.cesde.pps.model;
 
 import co.edu.cesde.pps.enums.AddressType;
 import java.util.Objects;
+import lombok.*;
 
 /**
  * Entidad Address - Representa direcciones de envío y/o facturación de un usuario.
@@ -25,6 +26,13 @@ import java.util.Objects;
  * - N:1 con User (muchas direcciones pertenecen a un usuario)
  * - 1:N con Order (como shipping_address_id o billing_address_id)
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
 public class Address {
 
     private Long addressId;
@@ -71,85 +79,7 @@ public class Address {
 
     // Getters y Setters
 
-    public Long getAddressId() {
-        return addressId;
-    }
 
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public AddressType getType() {
-        return type;
-    }
-
-    public void setType(AddressType type) {
-        this.type = type;
-    }
-
-    public String getLine1() {
-        return line1;
-    }
-
-    public void setLine1(String line1) {
-        this.line1 = line1;
-    }
-
-    public String getLine2() {
-        return line2;
-    }
-
-    public void setLine2(String line2) {
-        this.line2 = line2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Boolean getIsDefault() {
-        return isDefault;
-    }
-
-    public void setIsDefault(Boolean isDefault) {
-        this.isDefault = isDefault;
-    }
 
     // equals y hashCode basados en ID
 
