49457ebabf2ea62615417c3e23da0277a4a9dcda
daniel alejandro neira <danielgrc85@gmail.com>
2026-04-22 21:43:18 -0500

    implementacion try cash en Product Controller

--- src/main/java/co/edu/cesde/pps/controller/ProductController.java
+++ src/main/java/co/edu/cesde/pps/controller/ProductController.java
@@ -1,16 +1,42 @@
 package co.edu.cesde.pps.controller;
 
 
+import co.edu.cesde.pps.dto.ProductDTO;
+import co.edu.cesde.pps.model.Product;
 import co.edu.cesde.pps.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/products") // mapeo de la direccion (comunicacion con frontend)
 public class ProductController {
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
-        this.productService = productService;
+        this.productService = productService;  // se inyecta por constructor
 
     }
+
+    @GetMapping
+    public List<ProductDTO> getProducts() {
+        return productService.findAllProducts();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
+        try {
+            ProductDTO product = productService.findById(id);
+            return ResponseEntity.ok(product);
+
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body("Producto con id " + id + " no encontrado");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error interno del servidor");
+        }
+    }  // estudiar responseEntity (el mas Usado)  // estudiar la implementacion de Advise
 }
